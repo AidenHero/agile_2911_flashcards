b89856c921beae64438b75bd72fc845a58c040d8
Aiden D <aiden.denike@gmail.com>
2024-04-30 12:18:36 -0700

    adding files and folers

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from pathlib import Path
+from db import db
+from routes import
+
+app = Flask(__name__)
+app.register_blueprint(fill this with garbage)
+    
+
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.instance_path = Path("data").resolve()
+
+db.init_app(app)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8888)
---
+++ manage.py
@@ -0,0 +1,16 @@
+from db import db
+from app import app
+from models import Customer, Product, Order, ProductOrder
+from csv import DictReader
+from sqlalchemy.sql import functions as func
+import random
+
+def create_customer_database():
+    pass
+
+def create_flashcard_set_database():
+    pass
+
+def create_flashcard_database():
+    pass
+
