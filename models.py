cbda2419fc981c31dea695f49ce7d912e35000e3
Aiden D <aiden.denike@gmail.com>
2024-05-01 11:07:58 -0700

    some customer username password stuff

--- models.py
+++ models.py
@@ -7,15 +7,8 @@ from db import db
 
 class Customer(db.Model):
     id = mapped_column(Integer, primary_key=True)
-    # name = mapped_column(String(200), nullable=False, unique=True)
-    # phone = mapped_column(String(20), nullable=False)
-    # def to_json(self):
-    #  return {
-    #      'id': self.id,
-    #      'name': self.name,
-    #      'phone': self.phone,
-    #      'balance': self.balance
-    #      }
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
 
 class Flashcard_set(db.Model):
     set_id=mapped_column(Integer, primary_key=True)
