7e47fee7b00f25bddab06ecd3dbd6d7e40342747
Luis Saberon <luis.b.r.saberon@gmail.com>
2024-05-02 19:16:51 -0700

    Got user input for creating cards. Does nothing with the input yet because I forgot to set up the database.

--- routes/api_cards.py
+++ routes/api_cards.py
@@ -4,10 +4,17 @@ from models import Customer, Flashcard, Flashcard_set
 
 api_cards_bp = Blueprint('api_cards', __name__)
 
+@api_cards_bp.route('/', methods=["GET"])
+def make_card_screen():
+    return render_template("create_card.html")
+
 
 @api_cards_bp.route('/', methods=["POST"]) # to make new card
 def post_card():
-    pass
+    question = request.form['card_question']
+    answer = request.form['card_answer']
+    print(question, answer)
+    return render_template("create_card.html")
 
 @api_cards_bp.route('/<int:card_id>', methods=["PUT"]) # to update a card (using card_id)
 def put_card(card_id):
