5b9ac01f05d23ff5420f3e74bd646cd983e2d5a8
Karen <cj.karen02@gmail.com>
2024-05-05 17:02:09 -0700

    updated login, register html, added register

--- routes/api_sets.py
+++ routes/api_sets.py
@@ -2,11 +2,26 @@ from flask import Blueprint, render_template, redirect, url_for, request
 from db import db
 from models import Customer, Flashcard, Flashcard_set
 
+
 api_sets_bp = Blueprint('api_sets', __name__)
 
+@api_sets_bp.route('/', methods=["GET"])
+def make_set_screen():
+    return render_template("create_set.html")
+
+
+# # !! NOT FINISHED YET !! needs customer_id
 @api_sets_bp.route('/', methods=["POST"]) # to make new set
 def post_set():
-    pass
+    new_set_name = request.form['set_name']
+    new_set_descript = request.form['set_descript']
+
+    set = Flashcard_set(name=new_set_name, description=new_set_descript)
+    db.session.add(set)
+    db.session.commit()
+    print(new_set_name, new_set_descript)
+    return render_template("create_set.html")
+
 
 @api_sets_bp.route('/<int:set_id>', methods=["PUT"]) # to update a set (using set_id)
 def put_set(set_id):
--- routes/endpoint.py
+++ routes/endpoint.py
@@ -12,6 +12,20 @@ def homepage():
 def login(): 
     return render_template("login.html")
 
-@endpoint.route('/register')
+@endpoint.route('/register', methods=["GET"])
+def show_register_page(): 
+    return render_template("register.html")
+
+
+@endpoint.route('/register', methods=["POST"]) 
 def register(): 
+    new_name = request.form['register_name']
+    new_username = request.form['register_username']
+    new_password = request.form['register_password']
+
+    user = Customer(name=new_name, username=new_username, password=new_password)
+    db.session.add(user)
+    db.session.commit()
+
+    print(new_name, new_password, new_username)
     return render_template("register.html")
