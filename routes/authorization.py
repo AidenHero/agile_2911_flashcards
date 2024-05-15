827e99322871c01c0f7fa573aa286702b084feb8
Aiden D <aiden.denike@gmail.com>
2024-05-15 09:31:55 -0700

    feat/login working

--- routes/authorization.py
+++ routes/authorization.py
@@ -3,26 +3,30 @@ from db import db
 from models import Customer
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user
-from models import Customer
 
 authorization_bp = Blueprint("authorization", __name__)
 
 @authorization_bp.route('/login', methods=["POST"])
-def login():
-    user = Customer.query.filter_by(
-    username=request.form.get("username")).first()
-    if user.password == request.form.get("password"):
+def login_auth():
+    data = request.form.to_dict()
+    print(data)
+    print(request.form.get("loginusername"))
+    user = Customer.query.filter_by(username=request.form.get("loginusername")).first()
+    print(user)
+    # if user.password and request.form.get("loginPassword"): 
+    if user.password == request.form.get("loginPassword"):
         login_user(user)
-        return redirect(url_for("home"))
+        return redirect(url_for("pages.homepage"))
     return render_template("login.html")
 
 
-@authorization_bp.route('/register', methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        username=request.form.get("username")
-        password=request.form.get("password")
-        db.session.add()
+@authorization_bp.route('/register', methods=["POST"])
+def register_auth():
+    if request.form.get("register_name") and request.form.get("register_username") and request.form.get("register_password"):
+        user=Customer(name=request.form.get("register_name"),
+        username=request.form.get("register_username"),
+        password=request.form.get("register_password"))
+        db.session.add(user)
         db.session.commit()
-        return redirect(url_for("login"))
-    return render_template("sign_up.html")
+        return redirect(url_for('pages.login'))
+    return render_template("pages.show_register_page")
--- routes/endpoint.py
+++ routes/endpoint.py
@@ -8,7 +8,7 @@ endpoint = Blueprint('pages', __name__)
 def homepage():
     return render_template("home.html")
 
-@endpoint.route('/login')
+@endpoint.route('/login', methods=["GET"])
 def login(): 
     return render_template("login.html")
 
@@ -17,15 +17,15 @@ def show_register_page():
     return render_template("register.html")
 
 
-@endpoint.route('/register', methods=["POST"]) 
-def register(): 
-    new_name = request.form['register_name']
-    new_username = request.form['register_username']
-    new_password = request.form['register_password']
+# @endpoint.route('/register', methods=["POST"]) 
+# def register(): 
+#     new_name = request.form['register_name']
+#     new_username = request.form['register_username']
+#     new_password = request.form['register_password']
 
-    user = Customer(name=new_name, username=new_username, password=new_password)
-    db.session.add(user)
-    db.session.commit()
+#     user = Customer(name=new_name, username=new_username, password=new_password)
+#     db.session.add(user)
+#     db.session.commit()
 
-    print(new_name, new_password, new_username)
-    return render_template("register.html")
+#     print(new_name, new_password, new_username)
+#     return render_template("register.html")
